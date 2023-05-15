b50043791a080d03bb7ddcce67ff4194bf630fd8
Li <muhanli@ead.ubc.ca>
2023-05-15 10:32:09 -0700

    added node version of code

---
+++ misc/ses_sendmail_lambda.js
@@ -0,0 +1,52 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses"; // ES Modules import
+// const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses"); // CommonJS import
+
+exports.handler = async(event) => {
+    const CHARSET = 'UTF-8'
+    let   sender;
+    let   recipient;
+    let   content_text;
+    let   content_html;
+    let   subject;
+
+    const SESClient = new SESClient({
+        region: 'us-west-2'
+    });
+
+    const sendMailInput = {
+        Source: sender,
+        Destination: {ToAddresses: [recipient]},
+        Message: { 
+            Subject: { 
+                Data: subject,
+                Charset: CHARSET
+            },
+            Body: { 
+                Text: {
+                    Data: content_text,
+                    Charset: CHARSET
+                },
+                Html: {
+                    Data: content_html,
+                    Charset: CHARSET
+                },
+            },
+        },
+    }
+
+    try{
+        const sendEmail = new SendEmailCommand(sendMailInput);
+        const sendMailResp = await SESClient.send(sendEmail);
+        console.log(sendMailResp);
+
+        return{
+            statuscode: 200,
+            body: "SUCCESS"
+        }
+    }catch(err){
+        return{
+            statuscode: 400,
+            body: err
+        }
+    }
+}
