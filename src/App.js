b82b3ae6d4a4346daeb02c6c42eebcd200ccb142
Li <muhanli@ead.ubc.ca>
2023-05-17 15:54:55 -0700

    skeleton code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to My React Homepage!</h1>
+        {/* Add your components and content here */}
+      </div>
+    );
+  }
+}
+
+export default App;
