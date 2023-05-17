8da48ce704a85fceef04eed3466a3fcfcd9c7798
Li <muhanli@ead.ubc.ca>
2023-05-17 16:08:19 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import logo from './ubclogo.png'; // Replace 'logo.png' with the actual path to your logo image
 
-class App extends React.Component {
-  render() {
-    return (
+function App() {
+  return (
+    <>
       <div>
-        <h1>Welcome to My React Homepage!</h1>
-        {/* Add your components and content here */}
+        {/* First div at the top */}
+        <h2>This is the top div</h2>
+        <img src={logo} alt="Logo" style={{ width: '200px' }} />
+        {/* Add content for the first div */}
       </div>
-    );
-  }
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        {/* Second div below the first div */}
+        <h2>This is the bottom div</h2>
+        <button style={{ margin: '10px' }}>Button 1</button>
+        <button style={{ margin: '10px' }}>Button 2</button>
+        <button style={{ margin: '10px' }}>Button 3</button>
+      </div>
+      <h1>Welcome to My React Homepage!</h1>
+      {/* Add your components and content here */}
+    </>
+  );
 }
 
 export default App;
