53dcf835576b1e3aa39020d27fb930ee9a652586
Muhan Li <alanli8519@gmail.com>
2023-05-18 16:18:01 -0700

    updated home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './ubclogo.png';
+import './App.css'; // Import the CSS file
+
+import publicTable from './components/publicTable';
+import trackSample from './components/trackSample';
 
 function App() {
   return (
-    <>
-      <div>
-        {/* First div at the top */}
-        <h2>This is the top div</h2>
-        <img src={logo} alt="Logo" style={{ width: '200px' }} />
-        {/* Add content for the first div */}
+    <Router>
+      <div className="logoBackground">
+        <img src={logo} alt="Logo" style={{ height: '160px' }} />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-        {/* Second div below the first div */}
-        <h2>This is the bottom div</h2>
-        <button style={{ margin: '10px' }}>Button 1</button>
-        <button style={{ margin: '10px' }}>Button 2</button>
-        <button style={{ margin: '10px' }}>Button 3</button>
+      <div className="buttonBar">
+        <button className='navbutton'>Home</button>
+        <button className='navbutton'>View samples</button>
+        <button className='navbutton'>Track specific sample</button>
       </div>
-      <h1>Welcome to My React Homepage!</h1>
-      {/* Add your components and content here */}
-    </>
+      <Switch> 
+        <Route exact path="/"/>
+        <Route path="/Public" element={<publicTable/>}/>
+        <Route path="/Track" element={<trackSample/>}/>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
