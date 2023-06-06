deb34c0a380a8d3714cdfdfb3c43cda5d8c7be51
Li <muhanli@ead.ubc.ca>
2023-06-06 12:57:44 -0700

    updated resources in cdk directory

--- cdk/cdk-stack.ts
+++ cdk/cdk-stack.ts
@@ -1,6 +1,9 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_pinpoint as pinpoint } from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
@@ -17,6 +20,28 @@ export class CdkStack extends cdk.Stack {
     const pinpointApp = new pinpoint.CfnApp(this, 'HarmReduction', {
       name: 'HarmReduction'
     })
+
+    // OTP functionality
+    const OTPTable = new dynamodb.Table(this, 'OTPTable', {
+      partitionKey: { name: 'recipient', type: dynamodb.AttributeType.STRING },
+      // timeToLiveAttribute: "TTL"
+    });
+
+    const sendOTPFunction = new lambda.Function(this, 'sendOTP', {
+      timeout: cdk.Duration.seconds(600),
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'sendotp.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, './lambdas/sendotp')),
+    });
+
+    const verifyOTPFunction = new lambda.Function(this, 'verifyOTP', {
+      timeout: cdk.Duration.seconds(600),
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'verify.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, './lambdas/verifyotp')),
+    });
     
   }
 }
