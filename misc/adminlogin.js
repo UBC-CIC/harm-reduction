cbde17681b11a2c6c98e15ab1e7a5dcbbdc28a0a
Li <muhanli@ead.ubc.ca>
2023-06-09 15:54:09 -0700

    adminlogin

--- misc/adminlogin.js
+++ misc/adminlogin.js
@@ -5,14 +5,19 @@ export const handler = async(event) => {
     const initAuthCMD = new InitiateAuthCommand({
         AuthFlow: "USER_PASSWORD_AUTH", //"USER_SRP_AUTH" || "REFRESH_TOKEN_AUTH" || "REFRESH_TOKEN" || "CUSTOM_AUTH" || "ADMIN_NO_SRP_AUTH" || "USER_PASSWORD_AUTH" || "ADMIN_USER_PASSWORD_AUTH", // required
         AuthParameters: { 
-            "<keys>": "STRING_VALUE",
+            //"<keys>": "STRING_VALUE",
+            "username": "",
+            "password": ""
         },
         ClientId: "STRING_VALUE", // required
     })
 
     try{
-
+        const initAuthResp = await cognitoIdpClient.send(initAuthCMD);
+        console.log(initAuthResp);
+        return 'done';
     }catch(err){
-
+        console.log(err);
+        return err;
     }
 }
