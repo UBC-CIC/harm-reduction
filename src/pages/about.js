ef90c5d5510491dd098017cbd602faad755830fe
Muhan Li <alanli8519@gmail.com>
2023-07-28 17:42:56 -0700

    fixed image

--- src/pages/about.js
+++ src/pages/about.js
@@ -100,7 +100,7 @@ const About = () => {
                     width: WIDTH / 2,
                     }}
                     alt="Stock UBC image"
-                    src="ttps://images.unsplash.com/photo-1628863353691-0071c8c1874c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+                    src="https://images.unsplash.com/photo-1628863353691-0071c8c1874c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
                 />
             </Box>
             <Box
